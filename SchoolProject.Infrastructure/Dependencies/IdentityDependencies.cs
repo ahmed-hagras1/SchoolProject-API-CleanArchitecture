9a0ad115dbda34dd7f31050dfcec2a95bf651bf4
ahmed <hgahmed731@gmail.com>
2026-02-18 13:55:59 +0200

    Add Identity To SchoolProject

---
+++ SchoolProject.Infrastructure/Dependencies/IdentityDependencies.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolProject.Data.Entities.Identity;
+using SchoolProject.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrastructure.Dependencies
+{
+    public static class IdentityDependencies
+    {
+        public static IServiceCollection AddIdentityDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<User, IdentityRole<int>>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            })
+            .AddEntityFrameworkStores<AppDbContext>();
+
+            return services;
+        }
+    }
+}
