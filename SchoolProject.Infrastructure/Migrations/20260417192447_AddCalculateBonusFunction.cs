acf5a57aba1d706cd50bc307e3507faa3a06b279
ahmed <hgahmed731@gmail.com>
2026-04-17 21:31:31 +0200

    Dealing with Function

---
+++ SchoolProject.Infrastructure/Migrations/20260417192447_AddCalculateBonusFunction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalculateBonusFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE FUNCTION fn_CalculateBonus (@salary DECIMAL(18,2))
+        RETURNS DECIMAL(18,2)
+        AS
+        BEGIN
+            -- Just a simple example: 10% bonus
+            RETURN @salary * 0.10
+        END
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION fn_CalculateBonus");
+        }
+    }
+}
